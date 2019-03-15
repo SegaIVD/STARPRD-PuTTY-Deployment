225d53dc51f0cc1a6c9d6ba7f4d7fc70183d10e3
Sergei Shamsutdinov <sshamsut@atcwan.com>
2019-03-14 19:13:23 -0500

    Add project files.

---
+++ PuTTY Configurator/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuTTY_Configurator
+{
+    class Program
+    {
+        static int numerOfNics = 0;
+        static List<string> macAddrs = new List<string>();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press Enter to add STARPRD into registry");
+            Console.ReadLine();
+            Console.WriteLine("Addidng STARPRD session with Answerback to PuTTY\n");
+            ShowNetworkInterfaces();
+            Console.Write("\nEnter the interface number for Answerback (1-{0}): ", macAddrs.Count);
+            
+            int selection = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Answerback: " + macAddrs[selection - 1]);
+            Console.WriteLine("Addidng STARPRD session to PuTTY\n");
+            File.WriteAllText(Path.GetTempPath() + "\\STARPRD.reg", Resource1.STARPRD);
+            Process regeditProcess = Process.Start("regedit.exe", " /s " + Path.GetTempPath() + "\\STARPRD.reg");
+            regeditProcess.WaitForExit();
+
+            Microsoft.Win32.RegistryKey key;
+            key = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(@"Software\SimonTatham\PuTTY\Sessions\STARPRD");
+            key.SetValue("Answerback", macAddrs[selection - 1]);
+            key.Close();
+            Console.WriteLine("The Answerback key has been added to Registry");
+            File.Delete(Path.GetTempPath() + "\\STARPRD.reg");
+            Console.WriteLine("\nPress enter to exit...");
+            Console.ReadLine();
+        }
+
+        public static void ShowNetworkInterfaces()
+        {
+            IPGlobalProperties computerProperties = IPGlobalProperties.GetIPGlobalProperties();
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+            Console.WriteLine("Interface information for {0}.{1}     ",
+                    computerProperties.HostName, computerProperties.DomainName);
+            if (nics == null || nics.Length < 1)
+            {
+                Console.WriteLine("  No network interfaces found.");
+                return;
+            }
+
+            Console.WriteLine("  Number of interfaces .................... : {0}", nics.Length);
+            numerOfNics = nics.Length;
+            int num = 0;
+            foreach (NetworkInterface adapter in nics)
+            {
+                num++;
+                IPInterfaceProperties properties = adapter.GetIPProperties(); //  .GetIPInterfaceProperties();
+                Console.WriteLine();
+                Console.WriteLine(num + ") " + adapter.Description);
+                Console.WriteLine(String.Empty.PadLeft(adapter.Description.Length, '='));
+                Console.WriteLine("  Interface type .......................... : {0}", adapter.NetworkInterfaceType);
+                Console.Write("  Physical address ........................ : ");
+                PhysicalAddress address = adapter.GetPhysicalAddress();
+                byte[] bytes = address.GetAddressBytes();
+                StringBuilder mac = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    // Display the physical address in hexadecimal.
+                    Console.Write("{0}", bytes[i].ToString("X2"));
+                    mac.Append(bytes[i].ToString("X2"));
+                    // Insert a collumn after each byte, unless we are at the end of the 
+                    // address.
+                    if (i != bytes.Length - 1)
+                    {
+                        Console.Write(":");
+                    }
+                }
+                macAddrs.Add(mac.ToString());
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ PuTTY Deployment/MainWindow.xaml.cs
@@ -0,0 +1,227 @@
+﻿using IWshRuntimeLibrary;
+using System;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PuTTY_Deployment
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// Developed by Sergei Shamsutdinov
+    /// FedEx Supply Chain, 2019
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        NetworkInterface[] nics; //array for all NICs on PC
+        string macAddress; //MAC address string
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            //get computer properties
+            IPGlobalProperties computerProperties = IPGlobalProperties.GetIPGlobalProperties();
+
+            MainWindow1.Title = MainWindow1.Title + " on " + String.Format("{0}.{1}", computerProperties.HostName, computerProperties.DomainName); //change window's title text
+
+            //get all network interfaces
+            nics = NetworkInterface.GetAllNetworkInterfaces();
+
+            //add each NIC into a drop-down list
+            foreach (NetworkInterface adapter in nics)
+            {
+                comboBoxNIC.Items.Add(adapter.NetworkInterfaceType + " - " + adapter.Description.ToString());
+            }
+            comboBoxNIC.SelectedIndex = 0;
+        }
+
+        //ComboBox event
+        private void ComboBoxNIC_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            macAddress = nics[comboBoxNIC.SelectedIndex].GetPhysicalAddress().ToString(); //get MAC address
+            textBoxMAC.Text = macAddress;
+        }
+
+        //'Deploy' button event
+        private void ButtonDeploy_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder stringBuilder = new StringBuilder("The following sessions have been added to PuTTY: \n"); //create a new string for message
+            bool IsAdded = false;
+            if (checkBoxPrd.IsChecked.Value)
+            {
+                CreateSession(1, macAddress, checkBoxShortcut.IsChecked.Value); //create session
+                stringBuilder.AppendLine("STARPRD");
+                IsAdded = true;
+            }
+            if (checkBoxQa.IsChecked.Value)
+            {
+                CreateSession(2, macAddress, checkBoxShortcut.IsChecked.Value);
+                stringBuilder.AppendLine("STARQA");
+                IsAdded = true;
+            }
+            if (checkBoxStg.IsChecked.Value)
+            {
+                CreateSession(3, macAddress, checkBoxShortcut.IsChecked.Value);
+                stringBuilder.AppendLine("STARSTG");
+                IsAdded = true;
+            }
+            if (checkBoxTst.IsChecked.Value)
+            {
+                CreateSession(4, macAddress, checkBoxShortcut.IsChecked.Value);
+                stringBuilder.AppendLine("STARTST");
+                IsAdded = true;
+            }
+
+            //show message box
+            if (IsAdded)
+            {
+                //show added environments
+                MessageBox.Show(stringBuilder.ToString(), "Answerback: " + macAddress, MessageBoxButton.OK, MessageBoxImage.Information);
+            } else
+            {
+                //if nothing selected, show warning
+                MessageBox.Show("Select any enviroments", "PuTTY Sessions", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        //add session key into Windows Registry
+        private void CreateSession(int envType, string answerback, bool shortcut)
+        {
+            //envType - Numeric Code for WMS Environment
+            //answerback - MAC addr
+            //shortcut on Desktop = true/false
+            string envName, registryFileText;
+
+            //compile text for a new .reg file and create shortcuts for selected environments
+            switch (envType)
+            {
+                case 1:
+                    envName = "STARPRD"; // environment name
+                    registryFileText = Resource1.STARPRD; //get STARPRD regfile from resources
+                    if (shortcut)
+                    {
+                        CreateShortcut(envName, rbPublic.IsChecked.Value); //create PuTTY public/personal shortcut for STARPRD
+                    }
+                    break;
+                case 2:
+                    envName = "STARQA";
+                    registryFileText = Resource1.STARQA;
+                    if (shortcut)
+                    {
+                        CreateShortcut(envName, rbPublic.IsChecked.Value);
+                    }
+                    break;
+                case 3:
+                    envName = "STARSTG";
+                    registryFileText = Resource1.STARSTG;
+                    if (shortcut)
+                    {
+                        CreateShortcut(envName, rbPublic.IsChecked.Value);
+                    }
+                    break;
+                case 4:
+                    envName = "STARTST";
+                    registryFileText = Resource1.STARTST;
+                    if (shortcut)
+                    {
+                        CreateShortcut(envName, rbPublic.IsChecked.Value);
+                    }
+                    break;
+                default:
+                    return;
+            }
+            System.IO.File.WriteAllText(System.IO.Path.GetTempPath() + "\\" + envName + ".reg", registryFileText); //create .reg temp file and write all text into it
+
+            //import .reg file into the registry
+            Process regeditProcess = Process.Start("regedit.exe", " /s " + System.IO.Path.GetTempPath() + "\\" + envName + ".reg"); 
+            regeditProcess.WaitForExit();
+
+            //change Answerback regkey for the required environment
+            Microsoft.Win32.RegistryKey key;
+            key = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(@"Software\SimonTatham\PuTTY\Sessions\" + envName);
+            key.SetValue("Answerback", answerback); //set key
+            key.Close();
+            //remove .reg temp file
+            System.IO.File.Delete(System.IO.Path.GetTempPath() + "\\" + envName + ".reg");
+        }
+
+        //create shorcut on Public/User Desktop
+        private void CreateShortcut(string env, bool IsPublic)
+        {
+            object shDesktop = (object)"Desktop";
+            WshShell shell = new WshShell();
+            string shortcutAddress;
+            if (IsPublic)
+            {
+                //public shortcut full path with the environment name
+                shortcutAddress = Environment.GetFolderPath(Environment.SpecialFolder.CommonDesktopDirectory) + @"\" + env + ".lnk";
+            }
+            else
+            {
+                //personal shortcut path
+                shortcutAddress = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + @"\" + env + ".lnk";
+            }
+            //create a shortcut via WinAPI
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutAddress);
+            shortcut.Description = env + " session";
+            //shortcut.Hotkey = "None";
+            shortcut.Arguments = "-load " + env;
+            //set PuTTY folder
+            string puttyFolder = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86) + @"\Putty";
+
+            //check its folder
+            if (!System.IO.File.Exists(puttyFolder + @"\putty.exe"))
+            {
+                puttyFolder = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles) + @"\Putty";
+            }
+            if (!System.IO.File.Exists(puttyFolder + @"\putty.exe"))
+            {
+                MessageBox.Show("PuTTY is not installed", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            shortcut.WorkingDirectory = puttyFolder;
+            shortcut.TargetPath = puttyFolder + @"\putty.exe";
+            try
+            {
+                shortcut.Save();
+            }
+            catch (UnauthorizedAccessException) //Unable to save a shortcut, show error message
+            {
+                MessageBox.Show("Run as Administrator to create a shortcut on Public Desktop", "Access is denied", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        //logic for Check Boxes
+        private void CheckBoxAll_Checked(object sender, RoutedEventArgs e)
+        {
+            checkBoxPrd.IsChecked = true;
+            checkBoxQa.IsChecked = true;
+            checkBoxStg.IsChecked = true;
+            checkBoxTst.IsChecked = true;
+            checkBoxShortcut.IsChecked = true;
+        }
+
+        private void CheckBoxAll_Unchecked(object sender, RoutedEventArgs e)
+        {
+            checkBoxPrd.IsChecked = false;
+            checkBoxQa.IsChecked = false;
+            checkBoxStg.IsChecked = false;
+            checkBoxTst.IsChecked = false;
+            checkBoxShortcut.IsChecked = false;
+        }
+
+        private void CheckBoxShortcut_Checked(object sender, RoutedEventArgs e)
+        {
+            rbPublic.IsEnabled = true;
+            rbUser.IsEnabled = true;
+        }
+
+        private void CheckBoxShortcut_Unchecked(object sender, RoutedEventArgs e)
+        {
+            rbPublic.IsEnabled = false;
+            rbUser.IsEnabled = false;
+        }
+    }
+}
